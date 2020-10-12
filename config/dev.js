212bca5e290a1de540fa271c5e36af34f67a7c14
BrandonCorn <corn_brandon@columbusstate.edu>
2020-10-12 16:29:56 -0400

    add dev to git ignore

--- config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    PORT: 3000, 
-    DB: "mongodb+srv://BrandonCorn:Fbd3xmh2357@boundlessbookshelvesdev.equot.mongodb.net/BoundlessBookshelves?retryWrites=true&w=majority",
-
-}
