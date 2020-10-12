4a332b71867b6657474f72259f78f4de2af6b204
BrandonCorn <corn_brandon@columbusstate.edu>
2020-10-12 18:43:33 -0400

    fetching comic data and adding to page render

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 const express = require('express'); 
 const router = express.Router(); 
+const axios = require('axios');
 
 
-router.get('/', (req,res) => {
-    res.render('index'); 
+router.get('/', async (req,res) => {
+    let comic; 
+    await axios.get('http://xkcd.com/info.0.json')
+    .then(data => {
+        comic = data.data; 
+    })
+    .catch(err => console.log('could not retrieve comic')); 
+    console.log(comic);
+    const date = `${comic.month}/${comic.day}/${comic.year}` 
+    res.render('index', {
+        img: comic.img, 
+        title: comic.title,
+        description: comic.alt, 
+        date
+    }); 
 })
 
 router.get('/randomComic', require('./getRandomComic')); 
-
 
 module.exports = router; 
